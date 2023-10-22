0eab81bf1b9df1ff6a1e8a6cf6261fe83a1e67aa
Parallels <francois.gnz@gmail.com>
2023-10-22 22:12:09 +0200

    rotation pair impair forks

--- philo/src/forks.c
+++ philo/src/forks.c
@@ -6,7 +6,7 @@
 /*   By: fgonzale <fgonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 20:15:32 by fgonzale          #+#    #+#             */
-/*   Updated: 2023/10/17 20:16:21 by fgonzale         ###   ########.fr       */
+/*   Updated: 2023/10/22 21:57:39 by fgonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,9 +44,25 @@ int	take_forks(t_philo *philo)
 {
 	if (philo->data->philo_nb == 1)
 		return (one_philo_case(philo), 0);
-	if (!take_left_fork(philo))
-		return (0);
-	if (!take_right_fork(philo))
-		return (0);
+	if (philo->id % 2 == 0)
+	{
+		if (take_right_fork(philo) != 1)
+			return (0);
+		if (take_left_fork(philo) != 1)
+		{
+			drop_right_fork(philo);
+			return (0);
+		}
+	}
+	else
+	{
+		if (take_left_fork(philo) != 1)
+			return (0);
+		if (take_right_fork(philo) != 1)
+		{
+			drop_left_fork(philo);
+			return (0);
+		}		
+	}
 	return (1);
 }
