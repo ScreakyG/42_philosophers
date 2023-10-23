091f4f1f8ee2b589c3733c88afd0b5baabb0d1c9
Parallels <francois.gnz@gmail.com>
2023-10-23 14:53:43 +0200

    final

--- philo/src/sleep.c
+++ philo/src/sleep.c
@@ -6,7 +6,7 @@
 /*   By: fgonzale <fgonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 11:50:42 by fgonzale          #+#    #+#             */
-/*   Updated: 2023/10/17 20:09:19 by fgonzale         ###   ########.fr       */
+/*   Updated: 2023/10/23 02:07:51 by fgonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ void	sleep_eat_time(t_philo *philo)
 
 int	ft_usleep(time_t sleep_time, t_philo *philo)
 {
-	time_t	start;
+	time_t	wake;
 
-	start = get_time();
-	while (get_time() - start <= sleep_time)
+	wake = get_time() + sleep_time;
+	while (get_time() < wake)
 	{
 		if (get_philo_state(philo) == DEAD)
 			break ;
-		usleep(500);
+		usleep(100);
 	}
 	return (1);
 }
